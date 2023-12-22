d1b2e11b177371bac91412dad9524b6efa279384
bemoierian <bemoi.erian@gmail.com>
2023-12-22 22:51:41 +0200

    added fifth robot

--- src/multi-robot-exploration-pkg/scripts/odom_noise.py
+++ src/multi-robot-exploration-pkg/scripts/odom_noise.py
@@ -15,6 +15,8 @@ class NoisyOdom:
         self.pub2 = rospy.Publisher('/tb3_2/odom_noise', Odometry, queue_size=10)
         self.sub3 = rospy.Subscriber('/tb3_3/odom', Odometry, self.callback3)
         self.pub3 = rospy.Publisher('/tb3_3/odom_noise', Odometry, queue_size=10)
+        self.sub4 = rospy.Subscriber('/tb3_4/odom', Odometry, self.callback4)
+        self.pub4 = rospy.Publisher('/tb3_4/odom_noise', Odometry, queue_size=10)
 
     def callback0(self, msg):
         # Add Gaussian noise to position and orientation data
@@ -76,6 +78,21 @@ class NoisyOdom:
         msg.pose.pose.orientation.w += noise_orient[3]
 
         self.pub3.publish(msg)
+    def callback4(self, msg):
+        # Add Gaussian noise to position and orientation data
+        noise_pos = np.random.normal(0, 0.1, 3)  # Change 0.1 to desired standard deviation for position noise
+        noise_orient = np.random.normal(0, 0.01, 4)  # Change 0.01 to desired standard deviation for orientation noise
+
+        msg.pose.pose.position.x += noise_pos[0]
+        msg.pose.pose.position.y += noise_pos[1]
+        msg.pose.pose.position.z += noise_pos[2]
+
+        msg.pose.pose.orientation.x += noise_orient[0]
+        msg.pose.pose.orientation.y += noise_orient[1]
+        msg.pose.pose.orientation.z += noise_orient[2]
+        msg.pose.pose.orientation.w += noise_orient[3]
+
+        self.pub4.publish(msg)
 
     def run(self):
         rospy.spin()
